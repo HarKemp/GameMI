ea287ba97dc60c1f30815ca310c6fd471d00785d
Haralds <haralds.kempelis@edu.rtu.lv>
2024-03-01 20:49:26 +0200

    Gatava spēle (cilvēks pret cilvēku)

--- src/Game.java
+++ src/Game.java
@@ -80,50 +80,50 @@ public class Game {
 
     static boolean takeTurn(NumberString nS, int[] playerScores, int playerMove) {
 
-            // Izvada informāciju par spēles pašreizējo stāvokli
-            System.out.println("==========================================");
-            System.out.println("Numbers: " + nS.convertToString());
-            System.out.println("Player 1 points: " + playerScores[0]);
-            System.out.println("Player 2 points: " + playerScores[1]);
-            System.out.println("==========================================");
-            System.out.println("Player " + (playerMove + 1) + " move!");
+        // Izvada informāciju par spēles pašreizējo stāvokli
+        System.out.println("==========================================");
+        System.out.println("Numbers: " + nS.convertToString());
+        System.out.println("Player 1 points: " + playerScores[0]);
+        System.out.println("Player 2 points: " + playerScores[1]);
+        System.out.println("==========================================");
+        System.out.println("Player " + (playerMove + 1) + " move!");
 
 
-            // Veic gājienu
-            System.out.println("Choose move:");
-            System.out.println("(t): Take a number ");
-            System.out.println("(2): Split a Two");
-            System.out.println("(4): Split a Four");
-            char answer = InputReader.getChar();
-            int opponentPlayer;
-            switch (answer) {
-                case 't':
-                    System.out.print("Input number you want to take: ");
-                    int number = InputReader.getInt();
-                    if (!Move.takeNumber(nS, playerScores, playerMove, number)) {
-                        System.out.println("No " + number + "s in the string");
-                        return false;
-                    }
-                    break;
-                case '2':
-                    opponentPlayer = playerMove == 0 ? 1 : 0;
-                    if (!Move.splitNumber2(nS, playerScores, opponentPlayer)) {
-                        System.out.println("No twos in the string");
-                        return false;
-                    }
-                    break;
-                case '4':
-                    opponentPlayer = playerMove == 0 ? 1 : 0;
-                    if (!Move.splitNumber4(nS, playerScores, opponentPlayer)) {
-                        System.out.println("No fours in the string");
-                        return false;
-                    }
-                    break;
-                default:
-                    System.out.println("input-output error");
+        // Veic gājienu
+        System.out.println("Choose move:");
+        System.out.println("(t): Take a number ");
+        System.out.println("(2): Split a Two");
+        System.out.println("(4): Split a Four");
+        char answer = InputReader.getChar();
+        int opponentPlayer;
+        switch (answer) {
+            case 't':
+                System.out.print("Input number you want to take: ");
+                int number = InputReader.getInt();
+                if (!Move.takeNumber(nS, playerScores, playerMove, number)) {
+                    System.out.println("No " + number + "s in the string");
                     return false;
-            }
-            return true;
+                }
+                break;
+            case '2':
+                opponentPlayer = playerMove == 0 ? 1 : 0;
+                if (!Move.splitNumber2(nS, playerScores, opponentPlayer)) {
+                    System.out.println("No twos in the string");
+                    return false;
+                }
+                break;
+            case '4':
+                opponentPlayer = playerMove == 0 ? 1 : 0;
+                if (!Move.splitNumber4(nS, playerScores, opponentPlayer)) {
+                    System.out.println("No fours in the string");
+                    return false;
+                }
+                break;
+            default:
+                System.out.println("input-output error");
+                return false;
+        }
+        return true;
 
     }
 }
